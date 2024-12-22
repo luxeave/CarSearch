d29a2f47fc7eb5ecfdff1662d4afe6dc6e60ccc6
stephen antoni <luxeaves@gmail.com>
2024-12-22 10:44:40 +0700

    implement DTO

---
+++ src/main/java/com/example/carsearch/dto/CarDTO.java
@@ -0,0 +1,12 @@
+package com.example.carsearch.dto;
+
+public class CarDTO {
+    private Long id;
+    private String model;
+    private Integer lengthCm;
+    private Integer weightKg;
+    private Integer maxVelocityKmH;
+    private String color;
+
+    // Constructor, getters, setters
+}
