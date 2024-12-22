f1096f8df3ab1f1707793f9a544dfc5f55ed2d6a
stephen antoni <luxeaves@gmail.com>
2024-12-22 08:12:36 +0700

    initial commit

---
+++ src/test/java/com/example/carsearch/CarSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
