ba7cb35c64343285a78112f87665e1e040f42d88
stephen antoni <luxeaves@gmail.com>
2024-12-22 15:18:57 +0700

    implement DTO

--- src/main/java/com/example/carsearch/dto/CarDTO.java
+++ src/main/java/com/example/carsearch/dto/CarDTO.java
@@ -8,5 +8,60 @@ public class CarDTO {
     private Integer maxVelocityKmH;
     private String color;
 
-    // Constructor, getters, setters
+    public CarDTO(Long id, String model, Integer lengthCm, Integer weightKg, Integer maxVelocityKmH, String color) {
+        this.id = id;
+        this.model = model;
+        this.lengthCm = lengthCm;
+        this.weightKg = weightKg;
+        this.maxVelocityKmH = maxVelocityKmH;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getLengthCm() {
+        return lengthCm;
+    }
+
+    public void setLengthCm(Integer lengthCm) {
+        this.lengthCm = lengthCm;
+    }
+
+    public Integer getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(Integer weightKg) {
+        this.weightKg = weightKg;
+    }
+
+    public Integer getMaxVelocityKmH() {
+        return maxVelocityKmH;
+    }
+
+    public void setMaxVelocityKmH(Integer maxVelocityKmH) {
+        this.maxVelocityKmH = maxVelocityKmH;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
